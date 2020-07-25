bb50982e8e25b37de0f828a2b6a57024b4bacd4c
Alan Selewa <selewa@midway2-login2.rcc.local>
2020-07-24 19:06:00 -0500

    utils

--- R/torus.R
+++ R/torus.R
@@ -51,12 +51,12 @@ RunTorus <- function(torus_annot_file, torus_zscore_file, TORUS=system.file('tor
             'prior')
   
   res <- processx::run(command = TORUS, args = args, echo_cmd = TRUE, echo = TRUE)
-  enrich <- as_tibble(read.table(file = textConnection(res$stdout),skip=1,header=F,stringsAsFactors = F))
+  enrich <-  vroom::vroom(textConnection(res$stdout),skip = 1, col_names = F)
   colnames(enrich) <- c("term", "estimate", "low", "high")
   
   files <- list.files(path = 'prior/', pattern = '*.prior', full.names = T)
   res2 <- processx::run(command = 'cat', args=files)
-  snp_pip <- as_tibble(read.table(file = textConnection(res2$stdout),header=F,stringsAsFactors = F))
+  snp_pip <- vroom::vroom(textConnection(res2$stdout), col_names = F))
   colnames(snp_pip) <- c("snp","torus_pip")
   
   system('rm -rf prior/')
